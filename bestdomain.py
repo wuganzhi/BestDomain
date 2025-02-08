e401d1e8ec9e2b2fe3d06f579edf4a871ac3dd5e
rays <38714809+wuganzhi@users.noreply.github.com>
2025-02-08 11:26:01 +0800

    Update bestdomain.py

--- bestdomain.py
+++ bestdomain.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     # 示例URL和子域名对应的IP列表
     subdomain_ip_mapping = {
         'bestcf': 'https://ipdb.030101.xyz/api/bestcf.txt',
-        'bestproxy': 'https://ipdb.030101.xyz/api/bestproxy.txt',
+        # 'bestproxy': 'https://ipdb.030101.xyz/api/bestproxy.txt',
         # 添加更多子域名和对应的IP列表URL
     }
     
